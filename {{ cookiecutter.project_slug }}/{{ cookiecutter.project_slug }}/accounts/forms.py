92cf38ebf001a8cc1dcd483adef1b1d1bc1d8d83
Michael Karamuth <michael@confuzeus.com>
2022-01-08 18:19:12 +0400

    refactor: Use django-allcaptcha for captcha

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/accounts/forms.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/accounts/forms.py
@@ -1,28 +1,26 @@
 from allauth.account import forms as allauth_account_forms
 from django.contrib.auth import get_user_model
 
-from {{ cookiecutter.project_slug }}.core.mixins.form_mixins import HcaptchaFormMixin
-
-User = get_user_model()
+from allcaptcha.mixins import CaptchaFormMixin
 
 
-class LoginForm(HcaptchaFormMixin, allauth_account_forms.LoginForm):
+class LoginForm(CaptchaFormMixin, allauth_account_forms.LoginForm):
     pass
 
 
-class ResetPasswordForm(HcaptchaFormMixin, allauth_account_forms.ResetPasswordForm):
+class ResetPasswordForm(CaptchaFormMixin, allauth_account_forms.ResetPasswordForm):
     pass
 
 
 class ResetPasswordKeyForm(
-    HcaptchaFormMixin, allauth_account_forms.ResetPasswordKeyForm
+    CaptchaFormMixin, allauth_account_forms.ResetPasswordKeyForm
 ):
     pass
 
 
-class ChangePasswordForm(HcaptchaFormMixin, allauth_account_forms.ChangePasswordForm):
+class ChangePasswordForm(CaptchaFormMixin, allauth_account_forms.ChangePasswordForm):
     pass
 
 
-class SignupForm(HcaptchaFormMixin, allauth_account_forms.SignupForm):
+class SignupForm(CaptchaFormMixin, allauth_account_forms.SignupForm):
     pass
