32cf455201037012b1a61b0f601fb627a7919c51
Michael Karamuth <michael@confuzeus.com>
2022-01-09 03:42:25 +0400

    fix: Remove unused files and tests

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/core/context_processors.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/core/context_processors.py
@@ -4,6 +4,5 @@ from django.conf import settings
 def site_data(request):
     return {
         "django_debug": settings.DEBUG,
-        "captcha_site_key": settings.CAPTCHA_SITE_KEY,
         "allow_registration": settings.ACCOUNT_ALLOW_REGISTRATION,
     }
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/core/tests/test_utils.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/core/tests/test_utils.py
@@ -1,42 +1,12 @@
 import unittest.mock
 from unittest.mock import MagicMock
-from django.test import TestCase
+from django.test import testcase
 
-from {{ cookiecutter.project_slug }}.core.utils import captcha as captcha_util
 from {{ cookiecutter.project_slug }}.core.utils.urls import redirect_next_or_default
 
 
 
-class UtilsTests(TestCase):
-    def test_captcha(self):
-
-        h_captcha = captcha_util.Captcha(captcha_util.Captcha.H_CAPTCHA)
-        self.assertEqual(h_captcha.provider_url, captcha_util.Captcha.H_CAPTCHA_URL)
-
-        with self.assertRaises(NotImplementedError):
-            captcha_util.Captcha(captcha_util.Captcha.RECAPTCHA)
-
-        with unittest.mock.patch.object(captcha_util, "requests") as mock:
-            response = MagicMock()
-            mock.post.return_value = response
-
-            response.status_code = 200
-            response.json.return_value = {"success": True}
-
-            self.assertTrue(h_captcha.captcha_success("abcd"))
-
-            mock.post.side_effect = Exception()
-            self.assertFalse(h_captcha.captcha_success("abcd"))
-
-            mock.post.side_effect = None
-
-            response.json.side_effect = Exception()
-            self.assertFalse(h_captcha.captcha_success("abcd"))
-
-            response.json.side_effect = None
-
-            response.status_code = 400
-            self.assertFalse(h_captcha.captcha_success("abcd"))
+class TestUtils(testcase):
 
     def test_urls(self):
         response = redirect_next_or_default(path="/home/", default="/abcd/")
