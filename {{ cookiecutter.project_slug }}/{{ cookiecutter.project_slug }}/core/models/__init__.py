bb5076dd0aafd8d10e01feea5bedebf0c6005400
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-22 15:52:22 +0400

    Prepare for cookiecutter-less

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/core/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import accounts as accounts_models
-
-User = accounts_models.User
-UserProfile = accounts_models.UserProfile
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def healthcheck(request):  # pragma: no cover
-    return HttpResponse("OK")
