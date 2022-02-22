9d1e8844693d4666f864b5a0e837248f61adfad1
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-22 12:14:19 +0400

    Eradicate docker

--- {{ cookiecutter.project_slug }}/docker/piptools-compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-pip-compile --upgrade --generate-hashes --output-file base.txt base.in
-pip-compile --upgrade --generate-hashes --output-file dev.txt dev.in
-pip-compile --upgrade --generate-hashes --output-file prod.txt prod.in
