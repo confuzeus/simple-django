ad4cff4e196a60e2f7451fd086cd8df92d5b30ae
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-22 18:08:50 +0400

    Add useful pytest fixtures

---
+++ conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from django.contrib.auth import get_user_model
+from django.contrib.sites.models import Site
+from django.http import HttpRequest
+from faker import Faker
+
+User = get_user_model()
+
+
+@pytest.fixture
+def fake() -> Faker:
+    return Faker()
+
+
+@pytest.fixture
+def site(db) -> Site:
+    site = Site.objects.first()
+
+    if not site:
+        site = Site.objects.create(name=fake.word(), domain=fake.domain_name())
+
+    return
+
+
+@pytest.fixture
+def admin_request(admin_user, rf) -> HttpRequest:
+    request = rf.get("/")
+    request.user = admin_user
+    return
+
+
+@pytest.fixture
+def user(db, fake) -> User:
+    return User.objects.create(username=fake.user_name(), email=fake.ascii_email())
+
+
+@pytest.fixture
+def admin_user(db, fake) -> User:
+    return User.objects.create(
+        username=fake.user_name(), email=fake.ascii_email(), is_staff=True
+    )
