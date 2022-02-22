a73070a332bc7204d47e79186054c77dbe8e82f3
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-22 18:35:40 +0400

    Remove non-existant ctx processor

--- simple_django/core/context_processors.py
+++ simple_django/core/context_processors.py
@@ -4,5 +4,4 @@ from django.conf import settings
 def site_data(request):
     return {
         "django_debug": settings.DEBUG,
-        "allow_registration": settings.ACCOUNT_ALLOW_REGISTRATION,
     }
