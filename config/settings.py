d5789a460231636d47c89ddebb64be90cc6f7066
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-22 17:09:35 +0400

    Ignore E501

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,7 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
