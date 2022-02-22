4d4ced23b9a51e31ab12c69cd68185d59f11a94e
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-22 19:33:32 +0400

    Add project renamer

---
+++ rename.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+echo
+
+echo "What's your name?"
+
+read name
+
+echo "What's the name of the project?"
+
+read project_name
+
+echo "Project slug:"
+
+read project_slug
+
+echo "What's your domain name?"
+
+read domain_name
+
+echo "What's your email?"
+
+read email
+
+find . \
+    -type f \
+    -not -path '*/venv/*' \
+    -not -path '*/node_modules/*' \
+    -not -path '*/__pycache__/*' \
+    -not -path '*/fonts/*' \
+    -not -path '*pytest_cache*' \
+    -not -path '*/static_collected/*' \
+    -not -path '*git/*' \
+    -not -name 'rename.sh' \
+    -not -path '*/static/*' \
+    -exec sed -i "s/Josh Michael Karamuth/$name/g" {} \;\
+    -exec sed -i "s/Simple Django/$project_name/g" {} \;\
+    -exec sed -i "s/example.com/$domain_name/g" {} \;\
+    -exec sed -i "s/admin@$domain_name/$email/g" {} \;
+
+mv simple_django $project_slug
