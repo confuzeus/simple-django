6b418eec6e370ca66d5247aab9f9567b3fe1b5b9
Josh Michael Karamuth <michael@confuzeus.com>
2022-02-24 12:59:59 +0400

    Test ctx processors

---
+++ simple_django/core/tests/core/test_context_processers.py
@@ -0,0 +1,11 @@
+from simple_django.core.context_processors import site_data
+
+
+def test_site_data(rf, settings):
+    request = rf.get("/")
+
+    ctx = site_data(request)
+    assert "django_debug" in ctx.keys()
+
+    assert ctx['django_debug'] is settings.DEBUG
+
