8d25a7271c19a07e68e3fba9452b7b23d6c45aee
Josh Michael Karamuth <michael@confuzeus.com>
2022-04-01 22:10:54 +0400

    DRY up logging config

--- config/settings.py
+++ config/settings.py
@@ -359,64 +359,49 @@ MANAGERS = ADMINS
 # https://docs.djangoproject.com/en/3.2/ref/settings/#logging
 # See https://docs.djangoproject.com/en/3.2/topics/logging for
 # more details on how to customize your logging configuration.
-if DEBUG:
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "verbose": {
-                "format": "%(levelname)s %(asctime)s %(module)s "
-                "%(process)d %(thread)d %(message)s"
-            }
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "filters": {"require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}},
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
-        "handlers": {
-            "console": {
-                "level": "DEBUG",
-                "class": "logging.StreamHandler",
-                "formatter": "verbose",
-            }
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
         },
-        "root": {"level": "DEBUG", "handlers": ["console"]},
-    }
-else:
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "filters": {
-            "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}
-        },
-        "formatters": {
-            "verbose": {
-                "format": "%(levelname)s %(asctime)s %(module)s "
-                "%(process)d %(thread)d %(message)s"
-            }
-        },
-        "handlers": {
-            "mail_admins": {
-                "level": "ERROR",
-                "filters": ["require_debug_false"],
-                "class": "django.utils.log.AdminEmailHandler",
-            },
-            "console": {
-                "level": "DEBUG",
-                "class": "logging.StreamHandler",
-                "formatter": "verbose",
+    },
+    "root": {"level": "DEBUG", "handlers": ["console"]},
+}
+
+if not DEBUG:
+    LOGGING.update(
+        {
+            "root": {"level": "INFO", "handlers": ["console"]},
+            "loggers": {
+                "django.request": {
+                    "handlers": ["mail_admins"],
+                    "level": "ERROR",
+                    "propagate": True,
+                },
+                "django.security.DisallowedHost": {
+                    "level": "ERROR",
+                    "handlers": ["console", "mail_admins"],
+                    "propagate": True,
+                },
             },
         },
-        "root": {"level": "INFO", "handlers": ["console"]},
-        "loggers": {
-            "django.request": {
-                "handlers": ["mail_admins"],
-                "level": "ERROR",
-                "propagate": True,
-            },
-            "django.security.DisallowedHost": {
-                "level": "ERROR",
-                "handlers": ["console", "mail_admins"],
-                "propagate": True,
-            },
-        },
-    }
+    )
 
 # CACHES
 # ------------------------------------------------------------------------------
