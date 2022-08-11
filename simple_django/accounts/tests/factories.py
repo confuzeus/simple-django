d044718f2a3098f0d5d8259e08efeb3192e28c75
Josh Michael Karamuth <michael@confuzeus.com>
2022-08-11 09:29:30 +0400

    Add UserFactory

---
+++ simple_django/accounts/tests/factories.py
@@ -0,0 +1,24 @@
+import factory
+
+from simple_django.accounts.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    email = factory.Faker("email")
+
+    @factory.post_generation
+    def set_password(obj: User, create: bool, extracted: str, **kwargs):
+        password_plaintext = (
+            extracted
+            if extracted
+            else factory.Faker("password").evaluate(
+                None, None, extra={"locale": "en_US"}
+            )
+        )
+
+        obj.set_password(password_plaintext)
