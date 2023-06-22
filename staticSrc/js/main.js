e30adef7b3efb0edda4ccf8c69bac7e0d0e0d837
Josh Karamuth <hello@joshkaramuth.com>
2023-06-23 00:06:24 +0400

    Add new main.js

--- staticSrc/js/main.js
+++ staticSrc/js/main.js
@@ -1,37 +1,11 @@
-var app = (function(o) {
+import { Alert, Collapse } from "bootstrap";
 
-    const self = this;
+window.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll(".alert")
+    .forEach((alertNode) => new Alert(alertNode));
 
-    o.main = {
-
-        getCookie: function getCookie(name) {
-            let cookieValue = null;
-            if (document.cookie && document.cookie !== '') {
-                const cookies = document.cookie.split(';');
-                for (let i = 0; i < cookies.length; i++) {
-                    const cookie = cookies[i].trim();
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-                }
-            }
-            return cookieValue;
-        },
-
-        temporaryDisable: function (elem, timeout = 3000) {
-            elem.disabled = true;
-            setTimeout(() => {
-                elem.disabled = false;
-            }, timeout);
-        },
-
-        init: function initMain() {
-            window.onHcaptchaSubmit = this.onHcaptchaSubmit;
-        },
-
-    }
-
-    return o;
-})(app || {});
+  document
+    .querySelectorAll(".collapse")
+    .forEach((collapseNode) => new Collapse(collapseNode));
+});
