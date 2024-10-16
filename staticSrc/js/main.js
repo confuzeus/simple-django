5d610143e785a3082e19b56d62c53ed956d4537d
Josh Karamuth <hello@joshkaramuth.com>
2024-10-16 11:29:13 +0400

    Load all bootstrap js at once

--- staticSrc/js/main.js
+++ staticSrc/js/main.js
@@ -1,11 +1,5 @@
-import { Alert, Collapse } from "bootstrap";
+import { getCookie } from "./utils";
 
-window.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelectorAll(".alert")
-    .forEach((alertNode) => new Alert(alertNode));
-
-  document
-    .querySelectorAll(".collapse")
-    .forEach((collapseNode) => new Collapse(collapseNode));
-});
+window.App = {
+  getCookie,
+};
--- vendor-assets.sh
+++ vendor-assets.sh
@@ -8,6 +8,7 @@ export STATIC_DEST=static
 
 cp $VENDORS_ROOT/bootstrap-icons/bootstrap-icons.svg $STATIC_DEST/images/
 cp $VENDORS_ROOT/htmx.org/dist/htmx.min.js $STATIC_DEST/js/
+cp $VENDORS_ROOT/bootstrap/dist/js/bootstrap.bundle.min.js $STATIC_DEST/js/
 
 unset VENDORS_ROOT
 unset STATIC_DEST
